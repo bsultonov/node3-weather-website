9a13f6f27f842e98b4c2419464730274fb2b83f0
bositkhon <33827117+bositkhon@users.noreply.github.com>
2019-12-01 21:36:53 +0500

    The highest and lowest temperature info was added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longtitude, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + "% chance of rain.");
+            callback(undefined, body.daily.data[0].summary + ' The highest temperature for today is ' + body.daily.data[0].temperatureHigh + '. The lowest temperature for today is ' + body.daily.data[0].temperatureLow + '. It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + "% chance of rain.");
         }
     })
 }
